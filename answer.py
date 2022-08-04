b08402e918dfdaf257e645288f58f1229e801d6d
rodeok <nationx22@gmail.com>
2022-08-04 16:29:24 +0100

    answer but fix the error on line 1

---
+++ answer.py
@@ -0,0 +1,9 @@
+ num = input("input Number")
+ num2 = input("input Number")
+ solu = "number"
+ if num > num2:
+     print(f"{num} is greater")
+ elif num < num2:
+     print(f"{num} is less ")
+ else:
+     print(f"{num} is less than the given " + solu )
